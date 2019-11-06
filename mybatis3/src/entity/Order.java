123c42494db701651e07dffdaabf736ac2195680
i51chen <56567729+i51chen@users.noreply.github.com>
2019-11-06 09:12:02 +0800

    mybatis3+
    一对一关联
    <association 关联查询

---
+++ mybatis3/src/entity/Order.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class Order {
+	private int id;
+	private String orderNo;
+	private float price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
